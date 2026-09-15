e24658dc0fca9b72e2b372a8904537f06fb6ff17
whgusdn0955 <battlgusdn@gmail.com>
2026-09-15 18:42:20 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = "vocab-app-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => {
+
+        return cached ||
+          fetch(event.request)
+            .then(response => {
+
+              const copy = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then(cache =>
+                  cache.put(
+                    event.request,
+                    copy
+                  )
+                );
+
+              return response;
+
+            })
+            .catch(() =>
+              caches.match("./index.html")
+            );
+
+      })
+  );
+
+});
