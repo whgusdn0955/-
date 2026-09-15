25f7dcddccd15ca95fd1ffea13d22049ed3f6973
whgusdn0955 <battlgusdn@gmail.com>
2026-09-16 01:14:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
-const CACHE_NAME = "vocab-app-v1";
+const CACHE_NAME = "vocab-app-v1-0-0";
 
 const FILES = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 
   self.skipWaiting();
 });
 
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
@@ -34,35 +32,23 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => {
+    caches.match(event.request).then(cached => {
+      return cached ||
+        fetch(event.request)
+          .then(response => {
+            const copy = response.clone();
 
-        return cached ||
-          fetch(event.request)
-            .then(response => {
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, copy);
+            });
 
-              const copy = response.clone();
-
-              caches.open(CACHE_NAME)
-                .then(cache =>
-                  cache.put(
-                    event.request,
-                    copy
-                  )
-                );
-
-              return response;
-
-            })
-            .catch(() =>
-              caches.match("./index.html")
-            );
-
-      })
+            return response;
+          })
+          .catch(() => caches.match("./index.html"));
+    })
   );
-
 });
