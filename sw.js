5506e4bbd128c0f3d305424ef9e02ca7d9d3fe9d
whgusdn0955 <battlgusdn@gmail.com>
2026-09-16 16:40:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-const CACHE_VERSION="1.2.0";
-const CACHE_NAME=`vocab-app-${CACHE_VERSION.replaceAll(".","-")}`;
+const CACHE_NAME = "vocab-app-v2-0-0";
 
-const ASSETS=[
+const APP_FILES = [
   "./",
   "./index.html",
   "./style.css",
@@ -11,56 +10,69 @@ const ASSETS=[
   "./icon-512.png"
 ];
 
-self.addEventListener("install",event=>{
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache=>cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_FILES);
+    })
   );
 
   self.skipWaiting();
 });
 
-self.addEventListener("activate",event=>{
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then((keys) => {
+      return Promise.all(
         keys
-          .filter(key=>key!==CACHE_NAME)
-          .map(key=>caches.delete(key))
-      )
-    )
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
   );
 
   self.clients.claim();
 });
 
-self.addEventListener("fetch",event=>{
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
 
-  if(event.request.method!=="GET"){
+  const requestURL = new URL(
+    event.request.url
+  );
+
+  if (requestURL.origin !== self.location.origin) {
     return;
   }
 
   event.respondWith(
     fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
+      .then((response) => {
+        if (
+          response &&
+          response.status === 200 &&
+          response.type === "basic"
+        ) {
+          const copy = response.clone();
 
-        caches.open(CACHE_NAME)
-          .then(cache=>{
-            cache.put(
-              event.request,
-              copy
-            );
-          });
+          caches.open(CACHE_NAME).then(
+            (cache) => {
+              cache.put(
+                event.request,
+                copy
+              );
+            }
+          );
+        }
 
         return response;
       })
-      .catch(()=>
-        caches.match(event.request)
-          .then(cached =>
-            cached ||
-            caches.match("./index.html")
-          )
-      )
+      .catch(() => {
+        return caches.match(
+          event.request
+        );
+      })
   );
 });
