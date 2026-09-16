73d42bb2dfb65f64c215ee03f52f084c5a57ba5c
whgusdn0955 <battlgusdn@gmail.com>
2026-09-16 22:36:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "memorize-app-v2";
+const CACHE_NAME = "memorize-app-v6";
 
 const APP_FILES = [
   "./",
@@ -8,79 +8,39 @@ const APP_FILES = [
   "./manifest.json"
 ];
 
-self.addEventListener(
-  "install",
-  event => {
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then(cache =>
-          cache.addAll(APP_FILES)
-        )
-    );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
 
-    self.skipWaiting();
-  }
-);
+  self.skipWaiting();
+});
 
-self.addEventListener(
-  "activate",
-  event => {
-    event.waitUntil(
-      caches
-        .keys()
-        .then(keys =>
-          Promise.all(
-            keys
-              .filter(
-                key =>
-                  key !== CACHE_NAME
-              )
-              .map(key =>
-                caches.delete(key)
-              )
-          )
-        )
-    );
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 
-    self.clients.claim();
-  }
-);
+  self.clients.claim();
+});
 
-self.addEventListener(
-  "fetch",
-  event => {
-
-    if (
-      event.request.method !==
-      "GET"
-    ) {
-      return;
-    }
-
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-
-          const copy =
-            response.clone();
-
-          caches
-            .open(CACHE_NAME)
-            .then(cache =>
-              cache.put(
-                event.request,
-                copy
-              )
-            );
-
-          return response;
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return (
+        cachedResponse ||
+        fetch(event.request).catch(() => {
+          return caches.match("./index.html");
         })
-        .catch(() =>
-          caches.match(
-            event.request
-          )
-        )
-    );
-  }
-);
+      );
+    })
+  );
+});
