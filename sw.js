b62c308ceba00bfa702bbb4ab91e2949e91be91e
whgusdn0955 <battlgusdn@gmail.com>
2026-09-16 23:12:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "memorize-app-v7";
+const CACHE_NAME = "memorize-app-v8";
 
 const APP_FILES = [
   "./",
@@ -10,9 +10,12 @@ const APP_FILES = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_FILES);
-    })
+    caches.open(CACHE_NAME).then(
+      cache =>
+        cache.addAll(
+          APP_FILES
+        )
+    )
   );
 
   self.skipWaiting();
@@ -20,13 +23,21 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+          .filter(
+            key =>
+              key !==
+              CACHE_NAME
+          )
+          .map(key =>
+            caches.delete(
+              key
+            )
+          )
+      )
+    )
   );
 
   self.clients.claim();
@@ -34,14 +45,19 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) {
-        return cached;
-      }
-
-      return fetch(event.request).catch(() => {
-        return caches.match("./index.html");
-      });
+    caches.match(
+      event.request
+    ).then(cached => {
+      return (
+        cached ||
+        fetch(
+          event.request
+        ).catch(() =>
+          caches.match(
+            "./index.html"
+          )
+        )
+      );
     })
   );
 });
